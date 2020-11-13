e2fe3d662b8cf0e4640746d39f950be3d263d6e2
advanced373 <53189285+advanced373@users.noreply.github.com>
2020-11-13 10:30:25 +0200

    Add BMW class
    Signed-off-by: advanced373 <53189285+advanced373@users.noreply.github.com>

---
+++ BMW.java
@@ -0,0 +1,24 @@
+public class BMW implements ICar{
+    public int registrationNumber;
+    public int year;
+    public int HP;
+    public String color;
+    BMW()
+    {
+        this.registrationNumber=0;
+        this.year=0;
+        this.HP=0;
+        this.color="white";
+    }
+    BMW(int registrationNumber, int year, int HP, String color)
+    {
+        this.registrationNumber=registrationNumber;
+        this.year=year;
+        this.HP=HP;
+        this.color=color;
+    }
+    @Override
+    public void DisplayCharacteristics() {
+        System.out.println("Registration number:"+this.registrationNumber+"Color: "+this.color+" HP: "+this.HP + " year: "+this.year);
+    }
+}
