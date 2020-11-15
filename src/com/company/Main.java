b92dbd80fc5dbc2ad480025183a583d38b897ff1
vlijia_stefan_lab3 <vlijiastefan@gmail.com>
2020-11-15 17:08:04 +0200

    Modificare Main
    Am adaugat un meniu in Main structura try-catch pentru tratarea erorilor.
    
    Signed-off-by: vlijia_stefan_lab3 <vlijiastefan@gmail.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,58 @@
 package com.company;
+import java.io.*;
+
+import static java.lang.System.exit;
 
 public class Main {
-
     public static void main(String[] args) {
 	// write your code here
+        try {
+            ParcAuto myMobility=ParcAuto.getInstance();
 
-        ParcAuto myMobility=ParcAuto.getInstance();
+            myMobility.init("filename.txt");
 
-        myMobility.init("filename.txt");
+            int optiune = 0;
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            while(optiune <1 || optiune > 5)
+            {
+                System.out.println("Meniu:\n");
+                System.out.println("Afiseaza toate masinile - 1");
+                optiune = Integer.parseInt(br.readLine());
+                System.out.println("Adauga masina - 2");
+                optiune = Integer.parseInt(br.readLine());
+                System.out.println("Setare culoare - 3");
+                optiune = Integer.parseInt(br.readLine());
+                System.out.println("Cautare masina - 4");
+                System.out.println("Iesire din program - 5");
+                optiune = Integer.parseInt(br.readLine());
+            }
 
-        myMobility.printAll();
+            switch (optiune)
+            {
+                case 5:
+                    exit(0);
+                    break;
+                case 1:
+                    myMobility.printAll();
+                    break;
+                case 2:
+                    myMobility.addCar("BMW",34,2013,45,"white");
+                    myMobility.printAll();
+                    break;
+                case 3:
+                    myMobility.setColor(35,"black");
+                    myMobility.printAll();
+                    break;
+                case 4:
+                    myMobility.findCar(35);
+                    myMobility.printAll();
+                    break;
+                default: break;
+            }
 
-        myMobility.addCar("BMW",34,2013,45,"white");
-
-        myMobility.printAll();
-
-        myMobility.setColor(35,"black");
-
-        myMobility.findCar(35);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
 
     }
 }
