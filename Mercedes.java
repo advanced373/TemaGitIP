30676adf2ef595ee58aa4ea9c4e6f52a64c3a023
vlijia_stefan_lab3 <vlijiastefan@gmail.com>
2020-11-15 16:07:12 +0200

    Modificare greseala in clasa Audi si Mercedes
    Signed-off-by: vlijia_stefan_lab3 <vlijiastefan@gmail.com>

--- Mercedes.java
+++ Mercedes.java
@@ -4,20 +4,20 @@ public class Mercedes implements ICar{
     public void DisplayCharacteristics() {
         System.out.println("Caracteristicile masinii sunt:\n");
         System.out.println("Marca: Mercedes;\n");
-        System.out.println("Reg nr:"+regisitrationNumber+";\n");
+        System.out.println("Reg nr:"+ registrationNumber +";\n");
         System.out.println("Year:"+year+";\n");
         System.out.println("HP:"+HP+";\n");
         System.out.println("Color:"+color+";\n");
     }
 
-    public int regisitrationNumber;
+    public int registrationNumber;
     public int year;
     public int HP;
     public String color;
 
     public Mercedes(int reg_nr, int m_year, int hp, String m_color)
     {
-        regisitrationNumber = reg_nr;
+        registrationNumber = reg_nr;
         year= m_year;
         HP = hp;
         color = m_color;
